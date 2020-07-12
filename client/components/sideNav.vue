8436955fd7f67c324b19e41c7037beea54449de3
ashish336b <ashish336b@gmail.com>
2020-07-13 00:33:55 +0545

    small fixes on text alignment and responsiveness

--- client/components/sideNav.vue
+++ client/components/sideNav.vue
@@ -1,33 +1,44 @@
 <template>
   <div class="sidebar" id="sidebar">
     <div class="sidebar-heading">
-      <div class="has-text-primary has-text-centered">Admin Panel</div>
-      <div class="divider mt-4"></div>
+      <div class="has-text-primary has-text-centered">Medmandu</div>
     </div>
+    <div class="divider mt-4"></div>
     <aside class="menu">
       <ul class="menu-list">
         <li>
           <a class="active">
             <div class="list">
               <span class="iconify" data-icon="ic:twotone-dashboard" data-inline="false"></span>
-              <span>Dashboard</span>
+              <p>Dashboard</p>
             </div>
           </a>
         </li>
       </ul>
-      <div class="divider"></div>
-      <p class="menu-label has-text-weight-bold">Action</p>
+      <div class="divider mb-4"></div>
+      <p class="menu-label has-text-weight-bold">Profile</p>
+      <ul class="menu-list">
+        <li>
+          <a class="active">
+            <div class="list">
+              <span class="iconify" data-icon="ic:twotone-dashboard" data-inline="false"></span>
+              <p>Dashboard</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <p class="menu-label has-text-weight-bold">Ecommerce Section</p>
       <ul class="menu-list">
         <li>
           <a class="custom-list-container">
             <div class="list">
               <span class="iconify" data-icon="ic:twotone-dashboard" data-inline="false"></span>
-              <span>Manage</span>
+              <p>Invoice</p>
               <!-- <span class="iconify" data-icon="dashicons:arrow-down-alt2" data-inline="false"></span> -->
               <span class="iconify" data-icon="dashicons:arrow-right" data-inline="false"></span>
             </div>
           </a>
-          <ul>
+          <ul class="hide">
             <li>
               <a href="#">Member</a>
             </li>
@@ -40,12 +51,62 @@
           </ul>
         </li>
       </ul>
+
+      <p class="menu-label has-text-weight-bold">Ecommerce</p>
+      <ul class="menu-list">
+        <li>
+          <a class>
+            <div class="list">
+              <span class="iconify" data-icon="ri:medicine-bottle-line" data-inline="false"></span>
+              <p>Medicine</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <ul class="menu-list">
+        <li>
+          <a class>
+            <div class="list">
+              <span class="iconify" data-icon="ant-design:user-outlined" data-inline="false"></span>
+              <p>User</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <ul class="menu-list">
+        <li>
+          <a class>
+            <div class="list">
+              <span class="iconify" data-icon="mdi:warehouse" data-inline="false"></span>
+              <p>Inventory</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <ul class="menu-list">
+        <li>
+          <a class>
+            <div class="list">
+              <span class="iconify" data-icon="typcn:vendor-apple" data-inline="false"></span>
+              <p>Vendor</p>
+            </div>
+          </a>
+        </li>
+      </ul>
     </aside>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({})
+};
 </script>
 
 <style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,21 +4,9 @@
       <sideNav />
       <div class="main" id="main">
         <topNav />
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolorum iusto enim fugit minima voluptatibus unde animi magni placeat omnis, sed facere voluptate corporis tempore veritatis iure! Suscipit, voluptates numquam.</p>
+        <div>
+          <h1>hello world</h1>
+        </div>
       </div>
     </div>
   </div>
