7416249110b9aab65e63e2cb2b3bcb5c3db7ab6f
ashish336b <ashish336b@gmail.com>
2020-07-15 22:39:16 +0545

    github bot test

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <sidebar>
     <div class="container">
-      <button class="button is-danger">Hello world</button>
+      <button class="button is-danger">Hello world buddy</button>
     </div>
   </sidebar>
 </template>
