cce1f5efb00780a8828f6124601c24148c128385
ashish336b <ashish336b@gmail.com>
2020-07-19 13:10:33 +0545

    .

---
+++ client/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <sidebar>
+    <div class="container">
+      <button class="button is-danger">Hello world</button>
+    </div>
+  </sidebar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
