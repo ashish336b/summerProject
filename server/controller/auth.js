67a0b12f56846ed06463494825fa0c8e3fbc6e57
ashish336b <ashish336b@gmail.com>
2020-07-19 16:16:15 +0545

    admin authentication with jwt

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,10 +1,69 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
-router.get("/register", (req, res, next) => {});
+router.post("/register", async (req, res, next) => {
+  if (req.body.password !== req.body.confirmPassword)
+    return res.send({
+      error: true,
+      message: "Password does not Match",
+    });
+  /* check if phoneNumber already exits */
+  var getUser = await userModel.findOne({
+    username: req.body.username,
+  });
+  if (getUser)
+    return res.send({
+      error: true,
+      message: "User with this username already Exits",
+    });
+  /* hash password to store */
+  const hashedPassword = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
 
-router.get("/login", (req, res, next) => {
-  res.json({ success: true, message: "successfully login ok" });
+  new userModel({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    role: req.body.role,
+    address: req.body.address,
+    username: req.body.username,
+    password: hashedPassword,
+  })
+    .save()
+    .then((result) => {
+      res.json({
+        error: null,
+        message: "User Registered Successfully",
+        user: result,
+      });
+    });
+});
+
+router.get("/login", async (req, res, next) => {
+    const jwt = require('jsonwebtoken');
+    const user = await userModel.findOne({
+        username: req.body.username,
+        isDeleted: false,
+        role: 'admin',
+    });
+    if (!user) {
+        return res.status(403).json({
+            error: true,
+            message: "Password Or username doesn't Match",
+        });
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+        return res.status(403).json({
+            error: true,
+            message: "Password Or username doesn't Match",
+        });
+    }
+    user.password = null;
+    var token = await jwt.sign({ user: user }, '12helloworld12');
+    res.json({ token: token, error: null });
 });
 
 module.exports = router;
