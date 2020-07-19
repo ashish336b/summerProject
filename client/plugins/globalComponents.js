f5eabf6d9609bec5cad5a4581a65a43ee59f983b
ashish336b <ashish336b@gmail.com>
2020-07-19 13:06:43 +0545

    small changes

--- client/plugins/globalComponents.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import sidebar from '../components/sidebar';
-
-const components = {sidebar}
-Object.entries(components).forEach(([name, component]) => {
-   Vue.component(name, component)
- })
--- client/plugins/globaldatatable.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import { VuejsDatatableFactory } from "vuejs-datatable";
-
-Vue.use(VuejsDatatableFactory);
