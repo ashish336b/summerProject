facde2d7d9c231fc20a6775aaac4992c9d2b9a86
ashish336b <ashish336b@gmail.com>
2020-09-28 16:11:10 +0545

    small changes

--- server/controller/inventory.js
+++ server/controller/inventory.js
@@ -3,7 +3,7 @@ const inventoryModel = require("../models/inventoryModel");
 const paginate = require("../helpers/paginate");
 const prepareData = require("../helpers/prepareData");
 /**
- * method : GET
+ * method : GET ok
  * url : /crm/inventory/paginate
  */
 router.get("/paginate", async (req, res, next) => {
