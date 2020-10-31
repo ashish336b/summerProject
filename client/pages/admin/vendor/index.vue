82b46ece2ebc2ad75a52e05414ff18039eba09c2
ashish336b <ashish336b@gmail.com>
2020-10-31 14:11:34 +0545

    edit feature in vendor also

--- client/pages/admin/vendor/index.vue
+++ client/pages/admin/vendor/index.vue
@@ -1,5 +1,95 @@
 <template>
   <adminSidebar>
+    <div class="edit-container">
+      <div class="modal" :class="{ 'is-active': model.isActive }">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Edit Vendor</p>
+            <button
+              class="delete"
+              aria-label="close"
+              @click="
+                () => {
+                  model.isActive = false;
+                }
+              "
+            ></button>
+          </header>
+          <section class="modal-card-body">
+            <div class="columns is-multiline">
+              <div class="column is-6">
+                <div class="field">
+                  <label class="label">VendorName</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      v-model="model.modelData.vendorName"
+                      placeholder="Vendor Name"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="column is-6">
+                <div class="field">
+                  <label class="label">PhoneNumber</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      v-model="model.modelData.phoneNumber"
+                      placeholder="PhoneNumber"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="column is-6">
+                <div class="field">
+                  <label class="label">PAN Number</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      v-model="model.modelData.panNumber"
+                      placeholder="PAN Number"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="column is-6">
+                <div class="field">
+                  <label class="label">Address</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      v-model="model.modelData.address"
+                      placeholder="Address"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <footer class="modal-card-foot">
+            <button class="button is-success" @click="editInventory(model.id)">
+              Save changes
+            </button>
+            <button
+              class="button"
+              @click="
+                () => {
+                  model.isActive = false;
+                }
+              "
+            >
+              Cancel
+            </button>
+          </footer>
+        </div>
+      </div>
+    </div>
     <div>
       <div class="columns is-multiline">
         <div class="column is-11">
@@ -21,9 +111,10 @@
       :refresh="tableData.refresh"
     >
       <template slot-scope="item">
-        <button class="button is-primary is-small" @click="goToProfile(item)">
+        <a class="button is-primary is-small" @click="goToProfile(item)">
           Profile
-        </button>
+        </a>
+        <a class="button is-info is-small" @click="showForm(item)"> Edit </a>
       </template>
     </datatable>
   </adminSidebar>
@@ -31,11 +122,22 @@
 
 <script>
 import datatable from "@/components/datatable";
+import Swal from "sweetalert2";
 export default {
   components: {
     datatable,
   },
   data: () => ({
+    model: {
+      isActive: false,
+      id: "",
+      modelData: {
+        vendorName: "",
+        phoneNumber: "",
+        address: "",
+        panNumber: "",
+      },
+    },
     tableData: {
       params: null,
       refresh: true,
@@ -75,6 +177,26 @@ export default {
   methods: {
     goToProfile: function ({ item }) {
       this.$router.push("/admin/vendor/" + item._id);
+    },
+    showForm: function ({ item }) {
+      this.model.isActive = true;
+      this.model.id = item._id;
+      this.model.modelData.vendorName = item.vendorName;
+      this.model.modelData.phoneNumber = item.phoneNumber;
+      this.model.modelData.address = item.address;
+      this.model.modelData.panNumber = item.panNumber;
+    },
+    editInventory: function (id) {
+      this.$axios
+        .put(`/crm/vendor/${id}`, { ...this.model.modelData })
+        .then((result) => {
+          this.model.isActive = false;
+          Swal.fire(`Success!`, `Vendor Edit Successfully`, "success").then(
+            (result) => {
+              this.tableData.refresh = !this.tableData.refresh;
+            }
+          );
+        });
     },
   },
 };
